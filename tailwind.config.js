f0142abbcbbc4e33634054ff79c8a5462865ea3f
KadoNoDodo <kadonododo@gmail.com>
2025-02-08 16:18:20 +0100

    Added triangles to the index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.html"],  // Look for Tailwind classes in all HTML files in the root directory
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: ['./*.html', './*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
