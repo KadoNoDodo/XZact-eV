072b986f7741477b3e2e991c74443369009973ad
Kaspar Sinitsin <kaspar.sinitsin@students.beaminstitute.org>
2025-02-08 11:22:30 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],  // Look for Tailwind classes in all HTML files in the root directory
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
